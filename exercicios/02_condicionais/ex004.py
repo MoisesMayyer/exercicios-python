4cbc68a131dacaf16a144444c322e5972fab7be5
Mayyer <moisesmayer769@gmail.com>
2026-07-05 20:03:34 -0300

    refactor: reformulação do exercicio

--- exercicios/02_condicionais/ex004.py
+++ exercicios/02_condicionais/ex004.py
@@ -1,5 +1,10 @@
-km =float(input('qual a distancia da viagem: '))
-if km<=200:
-    print(f'para essa viagem vc gastara {0.50*km} de passagem')
+# exercício 4 - Calculo de distância de viagem
+
+distancia_viagem = float(input("Qual a distância da viagem (km)? "))
+
+if distancia_viagem <= 200:
+    valor_passagem = distancia_viagem * 0.50
+    print(f"Para essa viagem, você gastará R$ {valor_passagem:.2f} de passagem.")
 else:
-    print(f'como essa viagem é mais longa vc pagara {0.45* km} de passagem')
+    valor_passagem = distancia_viagem * 0.45
+    print(f"Como essa viagem é mais longa, você pagará R$ {valor_passagem:.2f} de passagem.")

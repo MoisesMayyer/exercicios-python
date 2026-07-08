e512b39f8310a80ccdbe483e21f530a3f32679ce
Mayyer <moisesmayer769@gmail.com>
2026-07-08 15:46:51 -0300

    refactor: melhoria de variaveis dos exercícios 005 e 005

--- exercicios/03_modulos_bibliotecas/ex004.py
+++ exercicios/03_modulos_bibliotecas/ex004.py
@@ -1,7 +1,11 @@
+# exercício 004 - sorteio de um aluno
+
 import random
-aluno1 = str(input('digite o nome do primeiro aluno'))
-aluno2 = str(input('digite o nome do segundo aluno'))
-aluno3 =str(input('digite o numero do terceiro aluno'))
-aluno4 = str(input('digite o nome do quarto aluno'))
-escolha = random.choice([aluno1,aluno2,aluno4])
-print('o aluno sorteado foi {}'.format(escolha))
+aluno1 = str(input('Digite o nome do primeiro aluno: '))
+aluno2 = str(input('Digite o nome do segundo aluno: '))
+aluno3 = str(input('Digite o nome do terceiro aluno: '))
+aluno4 = str(input('Digite o nome do quarto aluno: '))
+
+aluno_escolhido = random.choice([aluno1, aluno2, aluno3, aluno4])
+
+print(f'o aluno sorteado foi: {aluno_escolhido}')
--- exercicios/03_modulos_bibliotecas/ex005.py
+++ exercicios/03_modulos_bibliotecas/ex005.py
@@ -1,9 +1,14 @@
+# exercício 005 - listagem
 import random
-a1 = str(input('digite o primeiro nome '))
-a2 = str(input('digite o segundo nome '))
-a3 = str(input('digite o terceiro nome '))
-a4 = str(input('digite o quarto nome '))
-lista = [a1,a2,a3,a4]
-ordem= random.sample(lista, len(lista))
-print(lista)
+
+nome1 = str(input('Digite o primeiro nome: '))
+nome2 = str(input('Digite o segundo nome: '))
+nome3 = str(input('Digite o terceiro nome: '))
+nome4 = str(input('Digite o quarto nome: '))
+
+lista_nomes = [nome1,nome2,nome3,nome4]
+
+ordem_lista = random.sample(lista_nomes, len(lista_nomes))
+
+print(lista_nomes)
 

8d92ac1784e2079bb583e75f4b74842f40784ef5
Mayyer <moisesmayer769@gmail.com>
2026-07-18 22:53:50 -0300

    refactor: ex012

--- exercicios/04_loops/ex012.py
+++ exercicios/04_loops/ex012.py
@@ -1,6 +1,14 @@
-cont = 'S'
-while cont != 'N':
-    sex = str(input('digite seu sexo [M/F]')).strip().upper()
-    if sex != 'M' and sex != 'F':
-        cont= str(input(f'{sex} nn foi aceito. deseja tentar novamente [S/N]')).upper()
+while True:
+    sexo = input('Digite seu sexo [M/F]: ').strip().upper()
+
+    if sexo in ('M', 'F'):
+        print('Sexo cadastrado com sucesso!')
+        break
+
+    continuar = input(
+        f'"{sexo}" não é uma opção válida. Deseja tentar novamente? [S/N]: '
+    ).strip().upper()
+
+    if continuar == 'N':
+        breakS
 
